8a4eab5268f127e5f5b45c8f1de03fe860114bf9
cachemoi <m.rothierbautzer@gmail.com>
2021-11-21 12:20:14 +0000

    Use ETS cache to store features rather than Genserver state
    Storing state in a genserver means that we access cached feature flag values sequentially. Using ETS tables allows us to use concurrent reads to access this data, thereby fixing a performance bottleneck

--- lib/unleash/repo.ex
+++ lib/unleash/repo.ex
@@ -1,23 +1,36 @@
 defmodule Unleash.Repo do
-  @moduledoc false
+  @moduledoc """
+  This genserver polls the unleash service each time the given interval has
+  elapsed, refreshing both our local ETS cache and the backup state file if the
+  flag state has diverged.
+
+  The following configuration values are used:
+
+  Config.features_period(): polling interval - default 15 seconds
+  Config.retries(): number of time the call to refresh values is allowed to retry - default -1 (0)
+  """
+
   use GenServer
   require Logger
 
   alias Unleash.Config
   alias Unleash.Features
+  alias Unleash.Cache
 
   def init(%Features{} = features) do
+    Cache.init(features.features)
     {:ok, features}
   end
 
   def init(_) do
+    Cache.init()
     {:ok, %Features{}}
   end
 
   def start_link(state) do
     {:ok, pid} = GenServer.start_link(__MODULE__, state, name: Unleash.Repo)
 
-    unless Config.test? do
+    unless Config.test?() do
       initialize()
     end
 
@@ -25,84 +38,77 @@ defmodule Unleash.Repo do
   end
 
   def get_feature(name) do
-    GenServer.call(Unleash.Repo, {:get_feature, name})
+    Cache.get_feature(name)
   end
 
   def get_all_feature_names do
-    GenServer.call(Unleash.Repo, {:get_all_feature_names})
+    Cache.get_all_feature_names()
   end
 
-  def handle_call({:get_feature, name}, _from, state) do
-    feature = Features.get_feature(state, name)
-
-    {:reply, feature, state}
-  end
-
-  def handle_call({:get_all_feature_names}, _from, state) do
-    names = Features.get_all_feature_names(state)
-    {:reply, names, state}
-  end
-
-  def handle_info({:initialize, etag, retries}, state) do
+  def handle_info({:initialize, etag, retries}, _state) do
     if retries > 0 or retries <= -1 do
+      cached_features = %Features{features: Cache.get_features()}
+
       {etag, response} =
         case Unleash.Config.client().features(etag) do
-          :cached -> {etag, state}
+          :cached -> {etag, cached_features}
           x -> x
         end
 
-      features =
+      remote_features =
         case response do
           {:error, _} ->
-            state
-            |> read_state()
+            cached_features
+            |> read_file_state()
             |> schedule_features(etag, retries - 1)
 
           f ->
             schedule_features(f, etag)
         end
 
-      if features === state do
-        {:noreply, state}
+      if remote_features === cached_features do
+        {:noreply, nil}
       else
-        write_state(features)
+        Cache.upsert_features(remote_features.features)
+        write_file_state(remote_features)
+        {:noreply, nil}
       end
     else
       Logger.debug(fn ->
         "Retries === 0, disabling polling"
       end)
 
-      {:noreply, state}
+      {:noreply, nil}
     end
   end
 
   # https://github.com/appcues/mojito/issues/57
   # Work around for messages received from Mojito after we've passed over the timeout
   # threshold.
-  def handle_info({:mojito_response, _ref, _message}, state) do
-    {:noreply, state}
+  def handle_info({:mojito_response, _ref, _message}, _state) do
+    {:noreply, nil}
   end
 
-  defp read_state(%Features{features: []} = state) do
+  defp read_file_state(%Features{features: []} = cached_features) do
     if File.exists?(Config.backup_file()) do
       Config.backup_file()
       |> File.read!()
       |> Jason.decode!()
       |> Features.from_map!()
     else
-      state
+      cached_features
     end
   end
 
-  defp read_state(state), do: state
+  defp read_file_state(cached_features), do: cached_features
 
-  defp write_state(state) do
+  defp write_file_state(features) do
     if not File.dir?(Config.backup_dir()) do
       Config.backup_dir()
       |> File.mkdir_p!()
     end
 
-    content = Jason.encode_to_iodata!(state)
+    content = Jason.encode_to_iodata!(features)
 
     Config.backup_file()
     |> File.write!(content)
@@ -110,15 +116,13 @@ defmodule Unleash.Repo do
     Logger.debug(fn ->
       ["Wrote ", content, " to file ", Config.backup_file()]
     end)
-
-    {:noreply, state}
   end
 
   defp initialize do
     Process.send(Unleash.Repo, {:initialize, nil, Config.retries()}, [])
   end
 
-  defp schedule_features(state, etag, retries \\ Config.retries()) do
+  defp schedule_features(features, etag, retries \\ Config.retries()) do
     Logger.debug(fn ->
       retries_log =
         if retries >= 0 do
@@ -131,6 +135,6 @@ defmodule Unleash.Repo do
     end)
 
     Process.send_after(self(), {:initialize, etag, retries}, Config.features_period())
-    state
+    features
   end
 end
